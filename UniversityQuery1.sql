c6a843da059bf3b6c64fa07f35c27134c82178c9
Andrew Lanham <lanham.andrew09@gmail.com>
2020-10-03 20:19:20 -0600

    edits with quotes

--- UniversityQuery1.sql
+++ UniversityQuery1.sql
@@ -3,4 +3,4 @@ FROM Enrollments e
     INNER JOIN Students s ON e.SID=s.SID
     INNER JOIN Courses c ON e.CID=c.CID
 GROUP BY e.CID
-HAVING COUNT(s.Major != 'CS') < 10;
+HAVING COUNT(case s.Major when 'CS' then 1 else 0 end) < 10;
--- UniversityQuery4.sql
+++ UniversityQuery4.sql
@@ -10,7 +10,7 @@ AS
             FROM Enrollments
             WHERE SID=S2.SID AND CID=C.CID)
             AND S1.SID < S2.SID
-    EXCEPT
+    MINUS
         SELECT DISTINCT S1.SID AS SID1, S2.SID AS SID2
         FROM Students S1, Students S2, Projects P
         WHERE EXISTS (SELECT *
