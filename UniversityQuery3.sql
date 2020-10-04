1e96af3f6af9b99da3ae1a52357c5818760cecba
Andrew Lanham <lanham.andrew09@gmail.com>
2020-10-03 19:32:45 -0600

    edits

--- UniversityQuery3.sql
+++ UniversityQuery3.sql
@@ -3,46 +3,46 @@ SELECT *
 FROM Enrollments
 NATURAL JOIN Courses;
 
-SELECT DISTINCT "SID"
+SELECT DISTINCT 'SID'
 FROM Students S
 WHERE
 EXISTS (
     SELECT * FROM Classes
-    WHERE C_Name="EECS442"
-    AND "SID"=S."SID"
+    WHERE C_Name='EECS442'
+    AND 'SID'=S.'SID'
 )
 AND
 EXISTS (
     SELECT * FROM Classes
-    WHERE C_Name="EECS445"
-    AND "SID"=S."SID"
+    WHERE C_Name='EECS445'
+    AND 'SID'=S.'SID'
 
 )
 AND
 EXISTS (
     SELECT * FROM Classes
-    WHERE C_Name="EECS492"
-    AND "SID"=S."SID"
+    WHERE C_Name='EECS492'
+    AND 'SID'=S.'SID'
 )
 OR
 (
     EXISTS (
         SELECT * FROM Classes
-        WHERE C_Name="EECS482"
-        AND "SID"=S."SID"
+        WHERE C_Name='EECS482'
+        AND 'SID'=S.'SID'
     )
 AND
     EXISTS (
         SELECT * FROM Classes
-        WHERE C_Name="EECS486"
-        AND "SID"=S."SID"
+        WHERE C_Name='EECS486'
+        AND 'SID'=S.'SID'
     )
 )
 OR
     EXISTS (
         SELECT * FROM Classes
-        WHERE C_Name="EECS281"
-        AND "SID"=S."SID"
+        WHERE C_Name='EECS281'
+        AND 'SID'=S.'SID'
     )
 ORDER BY SID;
 
