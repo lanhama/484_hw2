0be012c45be123fd9a8f197da06475f017ac8cf2
Andrew Lanham <lanham.andrew09@gmail.com>
2020-10-06 09:42:06 -0600

    U2 edits

--- UniversityQuery2.sql
+++ UniversityQuery2.sql
@@ -1,13 +1,40 @@
-SELECT DISTINCT S.SID, S.Name
-FROM Students S, Members M
+-- SELECT DISTINCT S.SID, S.Name
+-- FROM Students S, Members M
+-- WHERE
+-- EXISTS (SELECT *
+--     FROM Members
+--     WHERE PID=M.PID AND SID != S.SID AND SID IN (
+--     SELECT DISTINCT S2.SID
+--     FROM Students S2
+--     WHERE EXISTS
+--         (SELECT *
+--         FROM Enrollments
+--             INNER JOIN Courses ON Enrollments.CID=Courses.CID
+--         WHERE SID=S2.SID AND C_Name='EECS280')
+--         AND EXISTS
+--         (SELECT *
+--         FROM Enrollments
+--             INNER JOIN Courses ON Enrollments.CID=Courses.CID
+--         WHERE SID=S2.SID AND (C_Name='EECS485') OR (C_Name='EECS484'))
+--         AND EXISTS
+--         (SELECT *
+--         FROM Enrollments
+--             INNER JOIN Courses ON Enrollments.CID=Courses.CID
+--         WHERE SID=S2.SID AND (C_Name='EECS483') OR (C_Name='EECS482'))
+--     )
+-- )
+--     AND
+--     EXISTS (SELECT *
+--     FROM
+--         Members
+--     WHERE SID=S.SID AND PID=M.PID);
+
+
+SELECT DISTINCT S1.SID, S1.Name
+FROM Students S1, Students S2, Members M
 WHERE
+S1.SID != S2.SID AND
 EXISTS (SELECT *
-    FROM Members
-    WHERE PID=M.PID AND SID != S.SID AND SID IN (
-    SELECT DISTINCT S2.SID
-    FROM Students S2
-    WHERE EXISTS
-        (SELECT *
         FROM Enrollments
             INNER JOIN Courses ON Enrollments.CID=Courses.CID
         WHERE SID=S2.SID AND C_Name='EECS280')
@@ -21,46 +48,13 @@ EXISTS (SELECT *
         FROM Enrollments
             INNER JOIN Courses ON Enrollments.CID=Courses.CID
         WHERE SID=S2.SID AND (C_Name='EECS483') OR (C_Name='EECS482'))
-    )
-)
     AND
     EXISTS (SELECT *
     FROM
         Members
-    WHERE SID=S.SID AND PID=M.PID);
-
-
--- SELECT DISTINCT S.SID, S.Name
--- FROM Students S, Members M
--- WHERE
--- EXISTS (SELECT *
---     FROM Members
---     WHERE
---     PID=M.PID AND SID != S.SID AND
---         SID IN (
---             SELECT DISTINCT S2.SID
---         FROM Enrollments E
---             INNER JOIN Students S2 ON E.SID=S2.SID
---             INNER JOIN Courses C ON E.CID=C.CID
---         WHERE EXISTS
---         (SELECT *
---             FROM Enrollments
---                 INNER JOIN Courses ON Enrollments.CID=Courses.CID
---             WHERE SID=S2.SID AND C_Name='EECS280')
---             AND EXISTS
---         (SELECT *
---             FROM Enrollments
---                 INNER JOIN Courses ON Enrollments.CID=Courses.CID
---             WHERE SID=S2.SID AND (C_Name='EECS485') OR (C_Name='EECS484'))
---             AND EXISTS
---         (SELECT *
---             FROM Enrollments
---                 INNER JOIN Courses ON Enrollments.CID=Courses.CID
---             WHERE SID=S2.SID AND (C_Name='EECS483') OR (C_Name='EECS482'))
--- )
--- )
---     AND
---     EXISTS (SELECT *
---     FROM
---         Members
---     WHERE SID=S.SID AND PID=M.PID);
+    WHERE SID=S1.SID AND PID=M.PID)
+    AND
+    EXISTS (SELECT *
+    FROM
+        Members
+    WHERE SID=S2.SID AND PID=M.PID);
