9d50d2e83ecfe2434ef2a24774812a09f8a5d747
Andrew Lanham <lanham.andrew09@gmail.com>
2020-10-06 09:35:39 -0600

    U2 edits

--- UniversityQuery2.sql
+++ UniversityQuery2.sql
@@ -1,5 +1,9 @@
-CREATE VIEW Students_taking_classes
-AS
+SELECT DISTINCT S.SID, S.Name
+FROM Students S, Members M
+WHERE
+EXISTS (SELECT *
+    FROM Members
+    WHERE PID=M.PID AND SID != S.SID AND SID IN (
     SELECT DISTINCT S2.SID
     FROM Students S2
     WHERE EXISTS
@@ -16,16 +20,7 @@ AS
         (SELECT *
         FROM Enrollments
             INNER JOIN Courses ON Enrollments.CID=Courses.CID
-        WHERE SID=S2.SID AND (C_Name='EECS483') OR (C_Name='EECS482'));
-
-SELECT DISTINCT S.SID, S.Name
-FROM Students S, Members M
-WHERE
-EXISTS (SELECT *
-    FROM Members
-    WHERE PID=M.PID AND SID != S.SID AND SID IN (
-        SELECT SID
-        FROM Students_taking_classes
+        WHERE SID=S2.SID AND (C_Name='EECS483') OR (C_Name='EECS482'))
     )
 )
     AND
@@ -33,8 +28,6 @@ EXISTS (SELECT *
     FROM
         Members
     WHERE SID=S.SID AND PID=M.PID);
-
-DROP VIEW Students_taking_classes;
 
 
 -- SELECT DISTINCT S.SID, S.Name
