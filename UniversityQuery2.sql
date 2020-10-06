c91c157399c09f3393663117a76c0b0865319a71
Andrew Lanham <lanham.andrew09@gmail.com>
2020-10-06 09:47:06 -0600

    U2 edits

--- UniversityQuery2.sql
+++ UniversityQuery2.sql
@@ -34,20 +34,20 @@ SELECT DISTINCT S1.SID, S1.Name
 FROM Students S1, Students S2, Members M
 WHERE
 S1.SID != S2.SID AND
-EXISTS (SELECT *
-        FROM Enrollments
-            INNER JOIN Courses ON Enrollments.CID=Courses.CID
-        WHERE SID=S2.SID AND C_Name='EECS280')
-        AND EXISTS
+    EXISTS (SELECT *
+    FROM Enrollments
+        INNER JOIN Courses ON Enrollments.CID=Courses.CID
+    WHERE SID=S2.SID AND C_Name='EECS280')
+    AND EXISTS
         (SELECT *
-        FROM Enrollments
-            INNER JOIN Courses ON Enrollments.CID=Courses.CID
-        WHERE SID=S2.SID AND (C_Name='EECS485') OR (C_Name='EECS484'))
-        AND EXISTS
+    FROM Enrollments
+        INNER JOIN Courses ON Enrollments.CID=Courses.CID
+    WHERE SID=S2.SID AND (C_Name='EECS485') OR (C_Name='EECS484'))
+    AND EXISTS
         (SELECT *
-        FROM Enrollments
-            INNER JOIN Courses ON Enrollments.CID=Courses.CID
-        WHERE SID=S2.SID AND (C_Name='EECS483') OR (C_Name='EECS482'))
+    FROM Enrollments
+        INNER JOIN Courses ON Enrollments.CID=Courses.CID
+    WHERE SID=S2.SID AND (C_Name='EECS483') OR (C_Name='EECS482'))
     AND
     EXISTS (SELECT *
     FROM
@@ -57,4 +57,5 @@ EXISTS (SELECT *
     EXISTS (SELECT *
     FROM
         Members
-    WHERE SID=S2.SID AND PID=M.PID);
+    WHERE SID=S2.SID AND PID=M.PID)
+    ORDER BY S1.Name DESC;
