adc5dcd2af2c5e8867fb5bc5c5f415e33e3bf132
Andrew Lanham <lanham.andrew09@gmail.com>
2020-10-06 22:21:56 -0600

    U2

--- UniversityQuery2.sql
+++ UniversityQuery2.sql
@@ -1,37 +1,5 @@
--- SELECT DISTINCT S.SID, S.Name
--- FROM Students S, Members M
--- WHERE
--- EXISTS (SELECT *
---     FROM Members
---     WHERE PID=M.PID AND SID != S.SID AND SID IN (
---     SELECT DISTINCT S2.SID
---     FROM Students S2
---     WHERE EXISTS
---         (SELECT *
---         FROM Enrollments
---             INNER JOIN Courses ON Enrollments.CID=Courses.CID
---         WHERE SID=S2.SID AND C_Name='EECS280')
---         AND EXISTS
---         (SELECT *
---         FROM Enrollments
---             INNER JOIN Courses ON Enrollments.CID=Courses.CID
---         WHERE SID=S2.SID AND (C_Name='EECS485') OR (C_Name='EECS484'))
---         AND EXISTS
---         (SELECT *
---         FROM Enrollments
---             INNER JOIN Courses ON Enrollments.CID=Courses.CID
---         WHERE SID=S2.SID AND (C_Name='EECS483') OR (C_Name='EECS482'))
---     )
--- )
---     AND
---     EXISTS (SELECT *
---     FROM
---         Members
---     WHERE SID=S.SID AND PID=M.PID);
-
-
 SELECT DISTINCT S1.SID, S1.Name
-FROM Students S1, Students S2, Members M
+FROM Students S1, Students S2, Projects M
 WHERE
 S1.SID != S2.SID AND
     EXISTS (SELECT *
@@ -42,12 +10,12 @@ S1.SID != S2.SID AND
         (SELECT *
     FROM Enrollments
         INNER JOIN Courses ON Enrollments.CID=Courses.CID
-    WHERE SID=S2.SID AND (C_Name='EECS485') OR (C_Name='EECS484'))
+    WHERE SID=S2.SID AND (C_Name='EECS485' OR C_Name='EECS484'))
     AND EXISTS
         (SELECT *
     FROM Enrollments
         INNER JOIN Courses ON Enrollments.CID=Courses.CID
-    WHERE SID=S2.SID AND (C_Name='EECS483') OR (C_Name='EECS482'))
+    WHERE SID=S2.SID AND (C_Name='EECS483' OR C_Name='EECS482'))
     AND
     EXISTS (SELECT *
     FROM
